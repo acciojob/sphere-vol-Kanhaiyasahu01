ac7d10ddfa88ef7cdf6de1087a87d40e795cbf22
Kanhaiya Lal Sahu <127585362+Kanhaiyasahu01@users.noreply.github.com>
2026-06-01 22:32:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+	const radius = document.querySelector("#radius");
+	const volume = document.querySelector("#volume");
+
+	const calculate = document.querySelector("#submit")
+
+	calculate.addEventListener("click", 
+							  ()=>{
+								  
+								  const vofs = 4/3 * 3.14 * Number(radius.value)*Number(radius.value) * Number(radius.value);
+									  volume.value = vofs;
+							  }
+							  )
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
